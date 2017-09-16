75eeb7351d8c2ae359844ef6964c92065c6dc6d0
ben eustace <awks.ben@gmail.com>
2017-09-16 22:24:32 +0100

    initial

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "os"
+    "github.com/gin-gonic/gin"
+    _ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+        
+        gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("index.html")       
+ 
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+        })       
+
+	router.Run(":" + port)
+}
